ea46bcd5b76e587dccbaa952a33dfe0f49a43100
Adrián Romero <adrianromero@users.noreply.github.com>
2014-04-03 11:07:06 +0200

    Porting Mimamememu to javafxgit statusgit status

--- start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set DIRNAME=%~dp0
-set CP="%DIRNAME%mimamememu.jar"
-
-start /B javaw -cp %CP% -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel -Djava.util.logging.config.file="%DIRNAME%logging.properties" -Ddirname.path="%DIRNAME%./" com.adr.mimame.Main %1
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-DIRNAME=`dirname $0`
-CLASSPATH=$DIRNAME/mimamememu.jar
-
-java -cp $CLASSPATH -Djava.util.logging.config.file=$DIRNAME/logging.properties -Ddirname.path=$DIRNAME/ com.adr.mimame.Main "$@"
